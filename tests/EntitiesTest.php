92b300fd2b956ee0d82d805c9de49ba64d5f99b9
Alexander Emelyanov <alexander.emelyanov@outlook.com>
2016-04-21 15:20:43 +0200

    Test coverage added for Deposit entity.

---
+++ tests/EntitiesTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Opteck\Tests;
+
+use Opteck\Entities\Deposit;
+use Opteck\Payload;
+use Opteck\Responses\GetDeposits;
+
+class EntitiesTest extends TestCase
+{
+    public function testDeposit()
+    {
+
+        // Valid deposits list with single deposit.
+        $json = <<<JSON
+{"returnCode":1,"description":"Successful call","timestampGenerated":"2016-04-21T16:08:12+03:00","data":{"allDepositsCount":"1","selectedDepositsCount":1,"limit":100,"offset":0,"deposits":[{"leadID":"1","dateDeposited":"2016-04-20T13:52:19+00:00","currency":"EUR","amount":"500.00","amountUSD":"574.71","isFirstTimeDeposit":"1","isValid":"1"}]}}
+JSON;
+        $response = new GetDeposits(new Payload($json));
+        $deposits = $response->getDeposits();
+
+        foreach ($deposits as $deposit) {
+            $this->assertEquals(1, $deposit->getLeadId());
+            $this->assertEquals(strtotime('2016-04-20T13:52:19+00:00'), $deposit->getDateDeposited());
+            $this->assertEquals('EUR', $deposit->getCurrency());
+            $this->assertEquals(500, $deposit->getAmount());
+            $this->assertEquals(574.71, $deposit->getAmountUSD());
+            $this->assertTrue($deposit->getIsFirstTimeDeposit());
+            $this->assertTrue($deposit->getIsValid());
+        }
+    }
+}
